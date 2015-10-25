343aa550ed0d7f76343a1888d363d76a11ca9119
Erin Moon <erinmoony@gmail.com>
2015-10-24 23:19:15 -0700

    Added starting position checker.

--- checkers.js
+++ checkers.js
@@ -15,14 +15,14 @@ function setSquare(player, row, col) {
 function getPieceAt(row, col) {
   if (checkerboard[row][col] === 'R' || 'B') {
       checkerboard[row][col] = null;
-  } else if (checkerboard[row][col] == null);
+  } else if (checkerboard[row][col] === null);
   return checkerboard;
 }
 //Sets a piece to null if it's "R" or "B". Else leaves it as null.//
 
 function clearBoard() {
   for (var i = 0; i < checkerboard[0].length; i++) {
-    for (var j = 0; j < checkerboard.length; j ++) {
+    for (var j = 0; j < checkerboard.length; j++) {
       checkerboard[i][j] = null;
     }
   }
@@ -47,7 +47,7 @@ function setUpRed() {
 function setUpBlack() {
   for (var j = (checkerboard.length - 1); j >= 5; j-=1) {
     for (var i = 0; i < checkerboard[0].length; i++) {
-      if (i%2 === 0 && j%2 !=+ 0) {
+      if (i%2 === 0 && j%2 !== 0) {
           checkerboard[j][i] = null;
       } else if (i%2 !== 0 && j%2 === 0) {
           checkerboard[j][i] = null;
@@ -57,3 +57,19 @@ function setUpBlack() {
 return checkerboard;
 }
 //Sets up Blacks side of the board.//
+
+var pieces={"Red":[[0,0],[0,2],[0,4],[0,6],[1,1],[1,3],[1,5],[1,7],[2,0],[2,2],[2,4],[2,6]],
+"Black":[[5,1],[5,3],[5,5],[5,7],[6,0],[6,2],[6,4],[6,6],[7,1],[7,3],[7,5],[7,7]]};
+//Shows position of all red and black pieces when they're in the starting positions.//
+
+pieces["Red"].map(function(piece){
+  var row = piece[0];
+  var col = piece[1];
+  return checkerboard[row][col] === "R";
+});
+
+pieces["Black"].map(function(piece){
+  var row = piece[0];
+  var col = piece[1];
+  return checkerboard[row][col] === "B";
+});

3bfcf37629538356a19ef26bbb801b36a6a4a6ba
chibitofu <erinmoony@gmail.com>
2015-11-17 11:32:26 -0800

    Fixed formatting.

--- checkers.js
+++ checkers.js
@@ -26,19 +26,19 @@ function clearBoard() {
       checkerboard[i][j] = null;
     }
   }
-    return checkerboard;
+  return checkerboard;
 }
 //Clears board of all piece, and sets them to null.//
 
 function setUpRed() {
   for (var i = 0; i < checkerboard.length; i++) {
-      for (var j = 0; j < 3; j++) {
-        if (i%2 !== 0 && j%2 === 0) {
-            checkerboard[j][i] = null;
-        } else if (i%2 === 0 && j%2 !== 0) {
-            checkerboard[j][i] = null;
-        } else (checkerboard[j][i] = "R");
-      }
+    for (var j = 0; j < 3; j++) {
+      if (i%2 !== 0 && j%2 === 0) {
+        checkerboard[j][i] = null;
+      } else if (i%2 === 0 && j%2 !== 0) {
+        checkerboard[j][i] = null;
+      } else (checkerboard[j][i] = "R");
+    }
   }
   return checkerboard;
 }
@@ -48,13 +48,13 @@ function setUpBlack() {
   for (var j = (checkerboard.length - 1); j >= 5; j-=1) {
     for (var i = 0; i < checkerboard[0].length; i++) {
       if (i%2 === 0 && j%2 !== 0) {
-          checkerboard[j][i] = null;
+        checkerboard[j][i] = null;
       } else if (i%2 !== 0 && j%2 === 0) {
-          checkerboard[j][i] = null;
+        checkerboard[j][i] = null;
       } else (checkerboard[j][i] = "B");
     }
   }
-return checkerboard;
+  return checkerboard;
 }
 //Sets up Blacks side of the board.//
 
